f343879ade11a41e4e804da59e38c63b5f755177
simondlevy <simon.d.levy@gmail.com>
2019-11-05 13:39:46 -0500

    Using CopterEnv in takeoff script

--- takeoff.py
+++ takeoff.py
@@ -7,11 +7,8 @@ Copyright (C) 2019 Simon D. Levy
 MIT License
 '''
 
-from time import sleep
+import gym
 import numpy as np
-
-from gym_copter.dynamics.quadxap import QuadXAPDynamics
-from gym_copter.dynamics import Parameters
 
 # Target 
 ALTITUDE_TARGET = 10
@@ -65,29 +62,14 @@ class AltitudePidController(object):
 
 if __name__ == '__main__':
 
-    # Dynamical parameters for copter
-    params = Parameters(
-
-            # Estimated
-            5.E-06, # b
-            2.E-06, # d
-
-            # https:#www.dji.com/phantom-4/info
-            1.380,  # m (kg)
-            0.350,  # l (meters)
-
-            # Estimated
-            2,      # Ix
-            2,      # Iy
-            3,      # Iz
-            38E-04, # Jr
-            15000)  # maxrpm
-
-    copter = QuadXAPDynamics(params)
+    # Create and initialize copter environment
+    env = gym.make('gym_copter:copter-v0')
+    env.reset()
 
     # initial conditions
     z = 0
     zprev = 0
+    u = np.zeros(4)
 
     # Create PID controller
     pid  = AltitudePidController(ALTITUDE_TARGET, ALT_P, VEL_P, VEL_I, VEL_D)
@@ -95,9 +77,12 @@ if __name__ == '__main__':
     # Loop until user hits the stop button
     while True:
 
-        # Extract altitude from state.  Altitude is in NED coordinates, so we negate it to use as input
+        # Update the environment with the current motor commands
+        state, _, _, _ = env.step(u*np.ones(4))
+
+         # Extract altitude from state.  Altitude is in NED coordinates, so we negate it to use as input
         # to PID controller.
-        z = -copter.getState().pose.location[2]
+        z = -state.pose.location[2]
 
         print('%+3.3f' % z)
 
@@ -110,14 +95,5 @@ if __name__ == '__main__':
         # Constrain correction to [0,1] to represent motor value
         u = max(0, min(1, u))
 
-        # Set motor values in sim
-        copter.setMotors(u*np.ones(4))
-
-        # Update the dynamics
-        copter.update(DT)
-
         # Update for first difference
         zprev = z
-
-        # Yield to Multicopter thread
-        sleep(.001)
