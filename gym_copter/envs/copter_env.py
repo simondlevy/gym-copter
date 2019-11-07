84735df83437dec9580c9ae8b7067af1df59eb7b
Levy <levys@wlu.edu>
2019-11-07 18:57:45 -0500

    Begin heading display

--- gym_copter/envs/copter_env.py
+++ gym_copter/envs/copter_env.py
@@ -39,7 +39,7 @@ class CopterEnv(gym.Env):
         obs = self.dynamics.getState()
 
         reward       = 0.0   # floating-point reward value from previous action
-        episode_over = False # whether it's time to reset the environment again (e.g., pole tipped over)
+        episode_over = False # whether it's time to reset the environment again (e.g., circle tipped over)
         info         = {}    # diagnostic info for debugging
 
         self.dynamics.update(self.dt)
@@ -51,24 +51,26 @@ class CopterEnv(gym.Env):
 
     def render(self, mode='human'):
 
-        # Adapted from https://raw.githubusercontent.com/openai/gym/master/gym/envs/classic_control/cartpole.py
+        # Adapted from https://raw.githubusercontent.com/openai/gym/master/gym/envs/classic_control/cartcircle.py
+
+        from gym.envs.classic_control import rendering
 
         W = 800
         H = 500
 
         if self.viewer is None:
 
-            from gym.envs.classic_control import rendering
-
             self.viewer = rendering.Viewer(W, H)
+
             sky = rendering.FilledPolygon([(0,H), (0,0), (W,0), (W,H)])
             sky.set_color(0.5,0.8,1.0)
-
             self.viewer.add_geom(sky)
+
             self.altitude_label = pyglet.text.Label('0000', font_size=24, x=600, y=300, 
                     anchor_x='left', anchor_y='center', color=(0,0,0,255))
             self.viewer.add_geom(_DrawText(self.altitude_label))
 
+            # Ground will be replaced on each call to render()
             self.ground = None
 
         # Detect window close
@@ -89,7 +91,6 @@ class CopterEnv(gym.Env):
         if not self.ground is None:
             del self.ground
 
-
         # Center top of ground quadrilateral depends on pitch
         y = H/2 * (1 + np.sin(rotation[1]))
 
@@ -100,6 +101,15 @@ class CopterEnv(gym.Env):
 
         # Draw new ground quadrilateral:         LL     LR     UR     UL
         self.ground = self.viewer.draw_polygon([(0,0), (W,0), (W,ury), (0,uly),], color=(0.5, 0.7, 0.3) )
+
+        res = 30
+        radius = 100
+        points = []
+        for i in range(res):
+            ang = 2*np.pi*i / res
+            points.append((np.cos(ang)*radius, np.sin(ang)*radius))
+
+        self.viewer.draw_polyline(points)
 
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
