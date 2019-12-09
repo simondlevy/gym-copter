c11ea498c7df3b20ed85c9e6792bb25637d96e5c
Levy <levys@wlu.edu>
2019-12-09 14:18:25 -0500

    Cleanup

--- gym_copter/envs/copter_realistic.py
+++ gym_copter/envs/copter_realistic.py
@@ -22,5 +22,5 @@ class CopterRealistic(CopterEnv):
         # Action space = motors
         self.action_space = spaces.Box(np.array([0,0,0,0]), np.array([1,1,1,1]))
 
-        # Observation space = full vehicle state
+        # Observation space = roll, pitch, yaw, altitude, groundspeed
         self.observation_space = spaces.Box(np.array([-90,-90,0,0,0]), np.array([+90,+90,359,1000,100])) 
