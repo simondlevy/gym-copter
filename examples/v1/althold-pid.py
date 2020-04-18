d9191cd5d4ad5f8bb5cee3782fda899283121448
simondlevy <simon.d.levy@gmail.com>
2020-04-17 20:14:14 -0400

    Added A2C script

--- examples/v1/althold-pid.py
+++ examples/v1/althold-pid.py
@@ -92,6 +92,8 @@ if __name__ == '__main__':
         # Update the environment with the current motor command, scaled to [-1,+1] and sent as an array
         s, r, _, _ = env.step([u])
 
+        env.render()
+
         # Extract altitude, vertical velocity from state
         z, v = s
 
