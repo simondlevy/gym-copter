f9dbfe2e9d0b1ec913c451d1532211c398848f33
simondlevy <simon.d.levy@gmail.com>
2020-04-24 17:21:45 -0400

    More leap

--- examples/leap.py
+++ examples/leap.py
@@ -9,37 +9,66 @@ MIT License
 
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 import gym_copter
 
-DURATION        = 20  # seconds
+DURATION        = 10  # seconds
 ALTITUDE_TARGET = 10 # meters
 
-# Create and initialize copter environment
-env = gym.make('Copter-v1')
-env.reset()
+def _subplot(t, x, k, label):
+    plt.subplot(2,1,k)
+    plt.plot(t, x)
+    plt.ylabel(label)
 
-# Start with motors full-throttle
-u = 1 * np.ones(4)
 
-# Loop for specified duration
-while True:
+if __name__ == '__main__':
 
-    # Get current time from environment
-    t = env.time()
+    # Create and initialize copter environment
+    env = gym.make('Copter-v1')
+    env.reset()
 
-    # Stop if time excedes duration
-    if t > DURATION: break
+    # Start with motors full-throttle
+    u = 1 * np.ones(4)
 
-    # Update the environment with the current motor command, scaled to [-1,+1] and sent as an array
-    s, r, _, _ = env.step(u)
+    # Initialize arrays for plotting
+    tvals = []
+    uvals = []
+    zvals = []
+    vvals = []
+    rvals = []
 
-    # Once we reach altitude, switch to forward motion
-    if -s[4] > ALTITUDE_TARGET:
-        u = np.array([0,1,0,1])
+    # Loop for specified duration
+    while True:
 
-    print(s)
+        # Get current time from environment
+        t = env.time()
 
-    # Display the environment
-    #env.render()
+        # Stop if time excedes duration
+        if t > DURATION: break
+
+        # Update the environment with the current motor command, scaled to [-1,+1] and sent as an array
+        s, r, _, _ = env.step(u)
+
+        # Once we reach altitude, switch to forward motion
+        z = -s[4]
+        if z > ALTITUDE_TARGET:
+            u = np.array([0,1,0,1])
+
+        # Track values
+        tvals.append(t)
+        uvals.append(u)
+        zvals.append(z)
+        rvals.append(r)
+
+        # Display the environment
+        #env.render()
+
+    # Plot results
+    _subplot(tvals, rvals, 1, 'Reward')
+    _subplot(tvals, zvals, 2, 'Altitude (m)')
+    plt.show()
+
+    # Cleanup
+    del env
 
