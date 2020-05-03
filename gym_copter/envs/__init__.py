d496f40017e8a01375a2489184b7690c8aeef5ae
simondlevy <simon.d.levy@gmail.com>
2020-05-03 15:50:49 -0400

    Renamed env.py => copter.py

--- gym_copter/envs/__init__.py
+++ gym_copter/envs/__init__.py
@@ -4,7 +4,7 @@ Copyright (C) 2019 Simon D. Levy
 MIT License
 '''
 
-from gym_copter.envs.env import CopterEnv
+from gym_copter.envs.copter import CopterEnv
 from gym_copter.envs.takeoff import CopterTakeoff
 from gym_copter.envs.distance import CopterDistance
 from gym_copter.envs.althold import CopterAltHold
