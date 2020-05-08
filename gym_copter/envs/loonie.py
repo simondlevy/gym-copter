0d6a6f1c3425aecb46ec38ab536ed3b22d9d87be
simondlevy <simon.d.levy@gmail.com>
2020-05-08 02:18:04 -0400

    Cleanup

--- gym_copter/envs/loonie.py
+++ gym_copter/envs/loonie.py
@@ -366,14 +366,8 @@ class LoonieLander(gym.Env, EzPickle):
 
         x= d.getState()
 
-        #s[0] is the horizontal coordinate
-        #s[1] is the vertical coordinate
-        #s[2] is the horizontal speed
-        #s[3] is the vertical speed
-        #s[4] is the angle
-        #s[5] is the angular speed
-
-        return x[d.STATE_Y], -x[d.STATE_Z], x[d.STATE_Y_DOT], -x[d.STATE_Z_DOT], x[d.STATE_PHI], x[d.STATE_PHI_DOT]
+        return self._pose_to_state(
+                x[d.STATE_Y], -x[d.STATE_Z], x[d.STATE_Y_DOT], -x[d.STATE_Z_DOT], x[d.STATE_PHI], x[d.STATE_PHI_DOT])
 
     def render(self, mode='human'):
 
