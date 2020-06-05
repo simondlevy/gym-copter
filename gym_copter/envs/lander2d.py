727502fff12303c193678f89e9e1fea84b2708b3
simondlevy <simon.d.levy@gmail.com>
2020-06-05 16:00:33 -0400

    Cleanup

--- gym_copter/envs/lander2d.py
+++ gym_copter/envs/lander2d.py
@@ -141,10 +141,13 @@ class CopterLander2D(gym.Env, EzPickle):
         if not (self.dynamics.landed() or self.resting_count):
             self.pose = posy, posz, -phi
 
+        #print((self.VIEWPORT_W/self.SCALE/2))
+        #exit(0)
+
         # Convert state to usable form
         state = np.array([
-            posy / (self.VIEWPORT_W/self.SCALE/2),
-            vely*(self.VIEWPORT_W/self.SCALE/2)/self.FPS,
+            posy / 10,
+            vely*10/self.FPS,
             (posz - self.ground_z) / (self.VIEWPORT_H/self.SCALE/2),
             velz*(self.VIEWPORT_H/self.SCALE/2)/self.FPS,
             phi,
