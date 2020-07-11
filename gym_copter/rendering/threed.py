1ddfae3d99984aa06a2d9a1162ac1679d0b5a02c
simondlevy <simon.d.levy@gmail.com>
2020-07-11 15:41:54 -0400

    More distance work

--- gym_copter/rendering/threed.py
+++ gym_copter/rendering/threed.py
@@ -221,11 +221,7 @@ class ThreeDDistanceRenderer(ThreeDRenderer):
 
         ThreeDRenderer.__init__(self, env, lim=5, label='Distance', viewangles=[30,120])
 
-        self.circle = create_axis(self.ax, 'r')
         pts = np.linspace(-np.pi, +np.pi, 1000)
-        self.circle_x = radius * np.sin(pts)
-        self.circle_y = radius * np.cos(pts)
-        self.circle_z = np.zeros(self.circle_x.shape)
 
     def render(self):
 
@@ -237,8 +233,7 @@ class ThreeDDistanceRenderer(ThreeDRenderer):
 
     def _update(self):
 
-        self.circle.set_data(self.circle_x, self.circle_y)
-        self.circle.set_3d_properties(self.circle_z)
+        return
 
     def _animate(self, _):
 
