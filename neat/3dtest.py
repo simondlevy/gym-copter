59c3981f7afa997e3110a2059f4ddf82d230df85
simondlevy <simon.d.levy@gmail.com>
2020-11-19 21:15:57 -0500

    Showing 3D animation

--- neat/3dtest.py
+++ neat/3dtest.py
@@ -11,19 +11,20 @@ import neat
 from neat_gym import read_file, eval_net
 from gym_copter.rendering.threed import ThreeDLanderRenderer
 import threading
-import numpy as np
-import time
 
 if __name__ == '__main__':
 
     # Load genome and configuration from pickled file
     genome, config = read_file()
 
+    # Make network from genome and configuration
     net = neat.nn.FeedForwardNetwork.create(genome, config)
 
+    # Create a three-D renderer
     renderer = ThreeDLanderRenderer(config.env)
 
-    thread = threading.Thread(target=eval_net, args=(net, config.env))
+    # Start the network-evaluation episode on a separate thread
+    thread = threading.Thread(target=eval_net, args=(net, config.env, True))
     thread.daemon = True
     thread.start()
 
