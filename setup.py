3af4376055924b9c4cddb32f61ac509ce343ec3b
Akash Patel <17132214+acxz@users.noreply.github.com>
2020-12-15 02:18:51 -0500

    add explicity dep on box2d-py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup (name = 'gym_copter',
     version = '0.1',
-    install_requires = ['gym', 'numpy'],
+    install_requires = ['gym', 'numpy', 'box2d-py'],
     description = 'Gym environment for multicopters',
     packages = ['gym_copter', 'gym_copter.envs', 'gym_copter.dynamics', 'gym_copter.rendering'],
     author='Simon D. Levy',
