fe6b514c7e99b13791d2d8eec6da6db65ea43689
simondlevy <simon.d.levy@gmail.com>
2020-12-17 04:37:44 -0500

    Make Lander-v4 be throttle/roll/pitch version

--- gym_copter/__init__.py
+++ gym_copter/__init__.py
@@ -25,6 +25,12 @@ register(
 )
 
 register(
+    id='Lander-v4',
+    entry_point='gym_copter.envs:Lander3DSimple',
+    max_episode_steps=2000
+)
+
+register(
     id='Distance-v0',
     entry_point='gym_copter.envs:Distance',
     max_episode_steps=1000
