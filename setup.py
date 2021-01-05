e1c6e572633c4065b3ea2fadb9a8aacc4f554c33
simondlevy <simon.d.levy@gmail.com>
2021-01-04 21:10:20 -0500

    Cleanup

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ Copyright (C) 2019 Simon D. Levy
 MIT License
 '''
 
-#from distutils.core import setup
 from setuptools import setup
 
-setup (name = 'gym_copter',
-    version = '0.1',
-    install_requires = ['gym', 'numpy', 'Box2D'],
-    description = 'Gym environment for multicopters',
-    packages = ['gym_copter', 'gym_copter.envs', 'gym_copter.dynamics', 'gym_copter.rendering'],
+setup(
+    name='gym_copter',
+    version='0.1',
+    install_requires=['gym', 'numpy', 'Box2D'],
+    description='Gym environment for multicopters',
+    packages=['gym_copter', 'gym_copter.envs',
+              'gym_copter.dynamics', 'gym_copter.rendering'],
     author='Simon D. Levy',
     author_email='simon.d.levy@gmail.com',
     url='https://github.com/simondlevy/gym-copter',
