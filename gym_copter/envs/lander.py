01afee502ea2d1f45259d087552302ee808a81b0
simondlevy <simon.d.levy@gmail.com>
2021-01-28 11:51:57 -0500

    Restored output

--- gym_copter/envs/lander.py
+++ gym_copter/envs/lander.py
@@ -200,9 +200,9 @@ class Lander(gym.Env, EzPickle):
 
             sleep(1./self.FRAMES_PER_SECOND)
 
-            # if (steps % 20 == 0) or done:
-            #     print('steps =  %03d    total_reward = %+0.2f' %
-            #           (steps, total_reward))
+            if (steps % 20 == 0) or done:
+                print('steps =  %03d    total_reward = %+0.2f' %
+                      (steps, total_reward))
 
             steps += 1
 
