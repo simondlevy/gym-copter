111e4dc1c80f3c6cedae43b75b9d9924e8aa0923
simondlevy <simon.d.levy@gmail.com>
2021-02-04 20:30:11 -0500

    More perspective work

--- gym_copter/sensors/vision/vs.py
+++ gym_copter/sensors/vision/vs.py
@@ -15,21 +15,20 @@ import cv2
 
 class VisionSensor(object):
 
-    def __init__(self, object_size, resolution, field_of_view):
+    def __init__(self, size, res, fov):
         '''
-        @param object_size meters
-        @param resolution pixels
-        @param field_of_view degrees
+        @param size size meters
+        @param res resolution in (pixels)
+        @param fov field of view (degrees)
         '''
 
-        self.object_size = object_size
-        self.resolution = resolution
-
-        self.tana = np.tan(np.radians(field_of_view/2))
+        self.size = size
+        self.res = res
+        self.fov = fov
 
     def get_image(self, x, y, z, phi, theta, psi):
 
-        image = np.zeros((self.resolution, )*2)
+        image = np.zeros((self.res, )*2)
 
         # Compute image center in pixels
         cx = self.locate(z, x)
@@ -43,22 +42,122 @@ class VisionSensor(object):
                           [-.25, +.5]])
 
         # Scale up the pentagon and center it in the image
-        shape *= self.scale(z, self.object_size)
+        shape *= self.scale(z, self.size)
         shape[:, 0] += cx
         shape[:, 1] += cy
 
         # Draw the shapegon as a filled polygon
         cv2.fillPoly(image, [shape.astype('int32')], 255)
 
+        '''
+        halfFovy = self.fov/2
+        d = VisionSensor.hypot(src.shape)
+        sideLength = int(d/np.cos(np.radians(halfFovy)))
+
+        # Compute warp matrix
+        M = getWarpMatrix(src.shape, psi, theta, phi)
+
+        # Warp image
+        return cv2.warpPerspective(src, M, (sideLength, sideLength))
+        '''
+
         return image
 
     def locate(self, z, coord):
 
-        return self.scale(z, coord) + self.resolution//2
+        return self.scale(z, coord) + self.res//2
 
     def scale(self, z, val):
 
-        return int(val * self.resolution / (2 * z * self.tana))
+        return int(val * self.res / (2 * z * np.tan(np.radians(self.fov/2))))
+
+    def getWarpMatrix(self, sz, psi, theta, phi):
+
+        st = np.sin(np.radians(psi))
+        ct = np.cos(np.radians(psi))
+        sp = np.sin(np.radians(theta))
+        cp = np.cos(np.radians(theta))
+        sg = np.sin(np.radians(phi))
+        cg = np.cos(np.radians(phi))
+
+        halfFovy = self.fov/2
+        d = VisionSensor.hypot(sz)
+        sideLength = d/np.cos(np.radians(halfFovy))
+        h = d/(2.0*np.sin(np.radians(halfFovy)))
+        n = h-(d/2.0)
+        f = h+(d/2.0)
+
+        # 4x4 transformation matrix F
+        F = np.zeros((4, 4))
+
+        # 4x4 rotation matrix around Z-axis by psi degrees
+        Rpsi = np.eye(4)
+
+        # 4x4 rotation matrix around X-axis by theta degrees
+        Rtheta = np.eye(4)
+
+        # 4x4 rotation matrix around Y-axis by phi degrees
+        Rphi = np.eye(4)
+
+        # 4x4 translation matrix along Z-axis by -h units
+        T = np.eye(4)
+
+        # 4x4 projection matrix
+        P = np.zeros((4, 4))
+
+        # Rpsi
+        Rpsi[0, 0] = Rpsi[1, 1] = ct
+        Rpsi[0, 1] = -st
+        Rpsi[1, 0] = st
+
+        # Rtheta
+        Rtheta[1, 1] = Rtheta[2, 2] = cp
+        Rtheta[1, 2] = -sp
+        Rtheta[2, 1] = sp
+
+        # Rphi
+        Rphi[0, 0] = Rphi[2, 2] = cg
+        Rphi[0, 2] = -sg
+        Rphi[2, 0] = sg
+
+        # T
+        T[2, 3] = -h
+
+        # P
+        P[0, 0] = P[1, 1] = 1.0/np.tan(np.radians(halfFovy))
+        P[2, 2] = -(f+n)/(f-n)
+        P[2, 3] = -(2.0*f*n)/(f-n)
+        P[3, 2] = -1.0
+
+        # Compose transformations
+        F = np.dot(np.dot(np.dot(np.dot(P, T), Rtheta), Rpsi), Rphi)
+
+        # Transform 4x4 points
+        halfW = sz[1]/2
+        halfH = sz[0]/2
+
+        ptsIn = np.array([-halfW, halfH, 0,
+                          halfW, halfH, 0,
+                          halfW, -halfH, 0,
+                          -halfW, -halfH, 0])
+
+        ptsInMat = np.reshape(ptsIn, (4, 1, 3))
+
+        ptsOutMat = cv2.perspectiveTransform(ptsInMat, F)  # Transform points
+
+        ptsInPt2f = np.zeros((4, 2)).astype('float32')
+        ptsOutPt2f = np.zeros((4, 2)).astype('float32')
+
+        for i in range(4):
+            ptsInPt2f[i] = ptsInMat[i, 0, :2] + np.array([halfW, halfH])
+            ptsOutPt2f[i] = ((ptsOutMat[i, 0, :2] +
+                             np.ones(2)) * (sideLength * 0.5))
+
+        return cv2.getPerspectiveTransform(ptsInPt2f, ptsOutPt2f)
+
+    @staticmethod
+    def hypot(shape):
+        return np.sqrt(shape[0]**2 + shape[1]**2)
 
     @staticmethod
     def display_image(image, name, scaleup=4):
