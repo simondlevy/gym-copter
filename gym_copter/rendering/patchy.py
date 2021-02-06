7738369edc0d01e043caf9ff5a1d5a6c53247a33
simondlevy <simon.d.levy@gmail.com>
2021-02-06 17:12:54 -0500

    Begin red patch

---
+++ gym_copter/rendering/patchy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+def main():
+
+
+    fig = plt.figure()
+    ax = plt.gca()
+    ax.set_aspect(1)
+
+    def init():
+        # initialize an empty list of cirlces
+        return []
+
+    def animate(i):
+
+        return [ax.add_patch(plt.Circle((0.5, 0.5), 0.45, color='r'))]
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=10, interval=20, blit=True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
