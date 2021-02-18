962628d9c3d2af7f89a5b5608a3fc4996f5ee9bd
simondlevy <simon.d.levy@gmail.com>
2021-02-17 23:47:09 -0500

    Support --raw option for copter-plot (no header or time values)

--- utils/copter-plot.py
+++ utils/copter-plot.py
@@ -8,25 +8,42 @@ MIT License
 '''
 
 import argparse
+from argparse import ArgumentDefaultsHelpFormatter
 import numpy as np
 import matplotlib.pyplot as plt
 
+from gym_copter.envs.lander import _Lander
 
 def main():
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+            formatter_class=ArgumentDefaultsHelpFormatter)
+
     parser.add_argument('csvfile', metavar='CSVFILE', help='input .csv file')
+
+    parser.add_argument('--raw', action='store_true',
+                        help='File has no header or timestamps')
+
     args = parser.parse_args()
 
     try:
-        data = np.genfromtxt(args.csvfile, delimiter=',', skip_header=1)
+        data = np.genfromtxt(args.csvfile, delimiter=',',
+                             skip_header=(0 if args.raw else 1))
     except Exception:
         print('Unable to open file %s' % args.csvfile)
         exit(1)
 
-    t = data[:, 0]
-    z = data[:, 5]
-    dz = data[:, 6]
+    zcol = 5
+    t = data[:,0]
+
+    if args.raw:
+        n = data.shape[0]
+        dur = n / _Lander.FRAMES_PER_SECOND
+        t = np.linspace(0, dur, n) if args.raw else data[:, 0]
+        zcol = 4
+
+    z = data[:, zcol]
+    dz = data[:, (zcol+1)]
 
     fig, axs = plt.subplots(2, 1, constrained_layout=True)
 
@@ -35,7 +52,7 @@ def main():
 
     fig.suptitle(args.csvfile, fontsize=16)
 
-    axs[1].plot(t, dz)
+    axs[1].plot(t, -dz)
     axs[1].set_xlabel('Time (s)')
     axs[1].set_ylabel('dZ/dt (m/s)')
 
