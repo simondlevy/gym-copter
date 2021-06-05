e036e842c4e6c579e6fdf3fe74fc5276ce226ec4
simondlevy <simon.d.levy@gmail.com>
2021-06-05 04:20:52 -0400

    Moved HUD demo to top folder

--- huddemo.py
+++ huddemo.py
@@ -7,13 +7,12 @@ Copyright (C) 2021 Simon D. Levy
 MIT License
 '''
 
-from gym_copter.envs.hover3d import Hover3D
 from gym_copter.envs.lander3d import Lander3D
 from gym_copter.rendering.hud import HUD
 
 def main():
 
-    env = Hover3D()
+    env = Lander3D()
 
     HUD(env)
 
