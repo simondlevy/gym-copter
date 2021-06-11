1e61e59882282f3ae6d40f97163971dc7f1b8d86
simondlevy <simon.d.levy@gmail.com>
2021-06-11 19:54:51 -0400

    =More Issue 79 work

--- heuristic/lander2d.py
+++ heuristic/lander2d.py
@@ -7,7 +7,6 @@ Copyright (C) 2019 Simon D. Levy
 MIT License
 '''
 
-from pidcontrollers import AngularVelocityPidController
 from pidcontrollers import DescentPidController
 from pidcontrollers import PositionHoldPidController
 
@@ -18,26 +17,19 @@ def heuristic(state, pidcontrollers):
 
     y, dy, z, dz, phi, dphi = state
 
-    rate_pid, poshold_pid, descent_pid = pidcontrollers
+    poshold_pid, descent_pid = pidcontrollers
 
-    phi_todo = 0
-
-    rate_todo = rate_pid.getDemand(dphi)
     pos_todo = poshold_pid.getDemand(y, dy)
-
-    phi_todo = rate_todo + pos_todo
 
     hover_todo = descent_pid.getDemand(z, dz)
 
-    return hover_todo-phi_todo, hover_todo+phi_todo
+    return hover_todo-pos_todo, hover_todo+pos_todo
 
 
 def main():
 
     demo2d('gym_copter:Lander2D-v0', heuristic,
-           (AngularVelocityPidController(),
-            PositionHoldPidController(),
-            DescentPidController()))
+           (PositionHoldPidController(), DescentPidController()))
 
 
 main()
