e0b5df1dbbd41441cf2100e47a7fa6b28302c0b0
simondlevy <simon.d.levy@gmail.com>
2021-06-11 19:39:46 -0400

    =More Issue 79 work

--- heuristic/pidcontrollers.py
+++ heuristic/pidcontrollers.py
@@ -59,16 +59,15 @@ class PositionHoldPidController:
 
         # Accumulated values
         self.lastError = 0
-        self.deltaError1 = 0
-        self.deltaError2 = 0
 
     def getDemand(self, x, dx):
 
         error = -x - dx
+
         deltaError = error - self.lastError
-        dterm = ((self.deltaError1 + self.deltaError2 + deltaError) * self.Kd)
-        self.deltaError2 = self.deltaError1
-        self.deltaError1 = deltaError
+
+        dterm = (error - self.lastError) * self.Kd
+
         self.lastError = error
 
         return dterm
