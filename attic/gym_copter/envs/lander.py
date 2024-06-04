11a1a5eb0d929ea190a1722e67dcb051cc35d302
simondlevy <simon.d.levy@gmail.com>
2024-06-04 12:42:31 -0400

    Cleanup

--- attic/gym_copter/envs/lander.py
+++ attic/gym_copter/envs/lander.py
@@ -10,6 +10,8 @@ MIT License
 import numpy as np
 
 from gym_copter.envs.task import _Task
+from gym_copter.sensors.vision.vs import VisionSensor
+from gym_copter.sensors.vision.dvs import DVS
 
 
 class Lander(_Task):
@@ -95,3 +97,42 @@ class Lander(_Task):
     def _get_motors(self, motors):
 
         return motors
+
+class LanderVisual(Lander):
+
+    RES = 16
+
+    def __init__(self, vs=VisionSensor(res=RES)):
+
+        Lander.__init__(self)
+
+        self.vs = vs
+
+        self.image = None
+
+    def step(self, action):
+
+        result = Lander.step(self, action)
+
+        x, y, z, phi, theta, psi = self.pose
+
+        self.image = self.vs.getImage(x,
+                                      y,
+                                      max(-z, 1e-6),  # keep Z positive
+                                      np.degrees(phi),
+                                      np.degrees(theta),
+                                      np.degrees(psi))
+
+        return result
+
+    def render(self, mode='human'):
+
+        if self.image is not None:
+            self.vs.display_image(self.image)
+
+
+class LanderDVS(LanderVisual):
+
+    def __init__(self):
+
+        LanderVisual.__init__(self, vs=DVS(res=LanderVisual.RES))
